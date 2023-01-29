01af5c5945f0810ff9d27f95371a6b37ff21384c
vitordcgomes <vitordcgomes@gmail.com>
2023-01-29 17:38:44 -0300

    limpeza de comentários obsoletos

--- main.cpp
+++ main.cpp
@@ -1,23 +1,6 @@
 //#include <Wire.h>
 #include <Servo.h>
 
-/*
-#define pin_serv 3
-Servo servo;
-
-void setup() {
-  servo.attach(pin_serv);
-}
-
-void loop() {
-  servo.write(40);
-  delay(200);
-  servo.write(140);
-  delay(200);
-}
-*/
-
-
 //Iniciando servos:
 // F: Frente; T: Tras; D: Direita; E: Esquerda;
 
@@ -156,85 +139,6 @@ float LeDistancia() {
 }
 
 void AndarReto() {
-  
-  /*
-  // andada funcional
-  //frente
-  FE_perna.write(posInicial_FE_perna - 30); //levanta (-40)
-  delay(150);
-  FE_corpo.write(posInicial_FE_corpo - 20); //-20
-  delay(150);
-  FE_perna.write(posInicial_FE_perna); //abaixa pos_inicial
-
-  //delay (250);
-  
-  //frente
-  FD_perna.write(posInicial_FD_perna - 30); //levanta
-  delay(150);
-  FD_corpo.write(posIncial_FD_corpo + 20); //+20
-  delay(150);
-  FD_perna.write(posInicial_FD_perna); //abaixa pos_inicial
-
-  //delay(250);
-
-  //frente
-  TE_perna.write(posInicial_TE_perna - 30); //levanta
-  delay(150);
-  TE_corpo.write(posInicial_TE_corpo - 30); //-40
-  delay(150);
-  TE_perna.write(posInicial_TE_perna); //abaixa pos_inicial
-
-  //delay(250);
-
-  //frente
-  TD_perna.write(posInicial_TD_perna - 30); //levanta
-  delay(150);
-  TD_corpo.write(posInicial_TD_corpo + 30); //+40
-  delay(150);
-  TD_perna.write(posInicial_TD_perna); //abaixa pos_inicial
-
-  //delay(250);
-
-/**************************************************************/
-  /*
-  //tras
-  FE_perna.write(posInicial_FE_perna + 30); //abaixa
-  delay(150);
-  FE_corpo.write(posInicial_FE_corpo + 30); //+40
-  delay(150);
-  FE_perna.write(posInicial_FE_perna); //levanta pos_inicial
-
-  //delay(250);
-
-  //tras
-  FD_perna.write(posInicial_FD_perna + 40); //abaixa
-  delay(150);
-  FD_corpo.write(posIncial_FD_corpo - 40); //-50
-  delay(150);
-  FD_perna.write(posInicial_FD_perna); //levanta pos_inicial
-
-  //delay(250);
-
-  //tras
-  TE_perna.write(posInicial_TE_perna + 20);
-  delay(150);
-  TE_corpo.write(posInicial_TE_corpo + 10); // +10
-  delay(150);
-  TE_perna.write(posInicial_TE_perna);
-
-  //delay(250);
-
-  //tras
-  TD_perna.write(posInicial_TD_perna + 25); //abaixa
-  delay(150);
-  TD_corpo.write(posInicial_TD_corpo - 10); //-30
-  delay(150);
-  TD_perna.write(posInicial_TD_perna); //levanta pos_inicial
-
-  //delay(250);
-  */
-
-  //teste 2 de andada
 
   //frente
   FE_perna.write(posInicial_FE_perna - 30); //levanta (-40)
@@ -297,6 +201,8 @@ void AndarReto() {
 }
 
 void GirarHorario() {
+  
+  //Realiza o movimento 3 vezes para garantir um giro de 90 graus, pelo menos.
   
   //frente
   FE_perna.write(posInicial_FE_perna - 30); //levanta (-40)
